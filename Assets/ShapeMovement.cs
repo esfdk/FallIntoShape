2d428ea19c50304b87851ca5894d4adb5159d69f
esfdk <jakobmelnyk@hotmail.com>
2014-02-04 14:38:18 +0100

    Early repo test thing

---
+++ Assets/ShapeMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+	// Update is called once per frame
+	void Update () {
+
+		if(true){
+			float rotationY = Input.GetAxis("Horizontal") * rotationSpeed;
+			float rotationZ = Input.GetAxis("Vertical") * rotationSpeed;
+			rotationZ *= Time.deltaTime;
+			rotationY *= Time.deltaTime;
+			transform.Rotate(rotationZ, rotationY, 0);
+		}
+		else
+		{
+			float translationX = Input.GetAxis("Horizontal") * speed;
+			float translationZ = Input.GetAxis("Vertical") * speed;
+			translationX *= Time.deltaTime;
+			translationZ *= Time.deltaTime;
+			transform.Translate(translationX, 0, translationZ);
+		}
+	}
+}

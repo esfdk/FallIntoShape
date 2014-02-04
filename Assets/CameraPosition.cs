98e594001305fbb16b75c5798576ca75ce187bad
esfdk <jakobmelnyk@hotmail.com>
2014-02-04 16:06:07 +0100

    added some hard surfaces and rotation based movement

---
+++ Assets/CameraPosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var p = GameObject.Find("Sphere").transform.position;
+		transform.position = new Vector3(p.x, p.y + 20, p.z);
+	}
+}
--- Assets/ShapeMovement.cs
+++ Assets/ShapeMovement.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class ShapeMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-
 	public float speed = 10.0F;
 	public float rotationSpeed = 100.0F;
-
+	private Transform dot;
+	// Use this for initialization
+	void Start () {
+		dot = GameObject.Find("dot").transform;
+	}
+	
 	// Update is called once per frame
 	void Update () {
-
 		if(true){
 			float rotationY = Input.GetAxis("Horizontal") * rotationSpeed;
 			float rotationZ = Input.GetAxis("Vertical") * rotationSpeed;
 			rotationZ *= Time.deltaTime;
 			rotationY *= Time.deltaTime;
 			transform.Rotate(rotationZ, rotationY, 0);
+			transform.position += transform.forward * Time.deltaTime *5;
 		}
 		else
 		{
@@ -27,7 +27,8 @@ public class ShapeMovement : MonoBehaviour {
 			float translationZ = Input.GetAxis("Vertical") * speed;
 			translationX *= Time.deltaTime;
 			translationZ *= Time.deltaTime;
-			transform.Translate(translationX, 0, translationZ);
 		}
+
+		//transform.position = Vector3.MoveTowards(transform.position, dot.position, 0.05f);
 	}
 }

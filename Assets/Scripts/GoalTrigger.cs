42252e9ae0d44a79134c000c6914935258827b26
esfdk <jakobmelnyk@hotmail.com>
2014-02-06 17:30:26 +0100

    Started fixing the star

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalTrigger : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "GoalTrigger")
+		{
+			var mgc = GameObject.Find("Main Camera").GetComponent<MainGameCode>();
+			mgc.NextObject();
+		}
+	}
+}
--- Assets/Scripts/MainGameCode.cs
+++ Assets/Scripts/MainGameCode.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class ShapeMovement : MonoBehaviour {
+public class MainGameCode : MonoBehaviour {
 
 	public float speed = 10.0F;
 	public float rotationSpeed = 100.0F;
 	public int activeObjectIndex = 0;
 	private List<GameObject> playObjects;
+	private List<Vector3> originalPositions;
+	private int currentScore = 0;
 
 	// Use this for initialization
 	void Start () 
 	{
 		playObjects = new List<GameObject>();
-		playObjects.Add(GameObject.Find("FirstObject(Cube)"));
+		playObjects.Add(GameObject.Find("FirstObject(Star)"));
 		playObjects.Add(GameObject.Find("SecondObject(Capsule)"));
+
+		originalPositions = new List<Vector3>();
+		foreach(var go in playObjects)
+		{
+			var p = go.transform.position;
+			originalPositions.Add(new Vector3(p.x, p.y, p.z));
+		}
 	}
 	
 	// Update is called once per frame
@@ -25,18 +34,31 @@ public class ShapeMovement : MonoBehaviour {
 		rotationZ *= Time.deltaTime;
 		rotationY *= Time.deltaTime;
 		playObjects[activeObjectIndex].transform.Rotate(rotationZ, rotationY, 0);
-		playObjects[activeObjectIndex].transform.position += transform.forward * Time.deltaTime *5;	
+		playObjects[activeObjectIndex].transform.position += playObjects[activeObjectIndex].transform.forward * Time.deltaTime *5;
 
 		// Camera movement
 		var p = playObjects[activeObjectIndex].transform.position;
-		transform.position = new Vector3(p.x, p.y + 20, p.z);
+		transform.position = new Vector3(p.x, p.y + 5, p.z);
 	}
 
-	void NextObject()
+	public void NextObject()
 	{
 		if(activeObjectIndex < playObjects.Count -1)
 		{
+			playObjects[activeObjectIndex].transform.position = originalPositions[activeObjectIndex];
 			activeObjectIndex++;
+			currentScore++;
 		}
+		else
+		{
+			currentScore++;
+
+		}
+
+	}
+
+	public void Reset()
+	{
+		currentScore = 0;
 	}
 }
